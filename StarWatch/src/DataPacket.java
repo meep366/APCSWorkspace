1e7e294d1fd1a7b0f08d9718f206cea4097057ef
Jack Croft <jackcroft2@yahoo.com>
2013-02-25 10:54:07 -0500

    StarWatch

---
+++ StarWatch/src/DataPacket.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author jack
+ *
+ */
+public interface DataPacket {
+	
+	 String getSource();
+	 int getQuadrant();
+	 double getIntelFactor();
+	 int getStrength();
+	 int getDuration();
+	
+
+}
