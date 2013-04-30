439625f6dbfa244b1587e35305e9b7b2783754c0
Jack Croft <jackcroft2@yahoo.com>
2013-04-30 17:12:39 -0400

    Yay APs!

---
+++ AP Exams/src/CookieOrder.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author jack
+ *
+ */
+public class CookieOrder {
+
+	/**
+	 * @param args
+	 */
+	
+	private String variety;
+	private int boxes;
+	
+	public CookieOrder(String variety, int numBoxes)
+	{
+		this.variety=variety;
+		boxes=numBoxes;
+	}
+	
+	public String getVariety()
+	{
+		return variety;
+	}
+	
+	public int getNumBoxes()
+	{
+		return boxes;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
